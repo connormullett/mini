b28a773be8b868e4011a0cd1931fc67589ca5734
Connor <connormullett@gmail.com>
2019-03-29 02:17:15 +0000

    curses up and running

--- mini.py
+++ mini.py
@@ -1,13 +1,25 @@
 
 import click
+import curses
 
+from curses import wrapper
+
+'''
+Make a file
+Enter a shell like environment
+Write text to file
+
+Write over mode
+Append mode
+Edit Mode
+'''
+
+def main():
+    stdscr = curses.initscr()
+    curses.endwin()
 
 @click.command()
 @click.argument('f')
 def mini(f):
-    click.echo('making %s' % f)
-
-
-if __name__ == '__main__':
-    mini()
+    main()
 
